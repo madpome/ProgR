defb822c4a258c943ce8ee2c623dde7340186044
Philippe EAR <earphilippefr@gmail.com>
2018-04-28 17:07:03 +0200

    début de merde

--- Occupe_Connection.java
+++ Occupe_Connection.java
@@ -4,16 +4,18 @@ public class Occupe_Connection implements Runnable {
    occupe_joueur et occupe_serveur
  */
 private int port;
-public void Occupe_Connection(int port){
+private Serveur serv;
+public void Occupe_Connection(int port, Serveur serv){
 	this.port = port;
+	this.serv = serv;
 }
 public void run(){
 	try{
 		ServerSocket serv = new ServerSocket(4000);
 		while(true) {
 			Socket so = serv.accept();
-			Occupe_Joueur oc_jo = new Occupe_Joueur(so);
-			Occupe_Serveur oc_se = new Occupe_Serveur(so);
+			Occupe_Joueur oc_jo = new Occupe_Joueur(so, serv);
+			Occupe_Serveur oc_se = new Occupe_Serveur(so, serv);
 			Thread t = new Thread(oc_jo);
 			Thread t2 = new Thread(oc_se);
 			t.start();
--- com_udp.c
+++ com_udp.c
@@ -1,46 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main(int argc, char ** argv){
-
-}
-//Verifie que c'est un ALL? ou un SEND? correcte
-int check_msg(char *s){
-    if(strlen(s)<4){
-        return -1;
-    }
-    int n = strlen(s);
-    char *p = malloc(5*sizeof(char));
-    memcpy(p,s,4*sizeof(char));
-    p[4] = '\0';
-    if((s[n-1] == '*' && s[n-2] == '*' && s[n-3] == '*')){
-        return -1;
-    }
-    if(strcmp(p,"SEND?") != 0){
-        p[3] = '\0';
-        if(strcmp(p, "ALL?") != 0){
-            return -1;
-        }else{
-            if(s[4] == ' '){
-                return 1;
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <unistd.h>
+void receive(int port, int portMulti, char *ipMulti){
+    //A déplacer dans le fork
+    int pid = fork();
+    if(pid == 0){
+        //Reception de messages normaux
+        int sock=socket(PF_INET,SOCK_DGRAM,0);
+        sock=socket(PF_INET,SOCK_DGRAM,0);
+        struct sockaddr_in address_sock;
+        address_sock.sin_family=AF_INET;
+        address_sock.sin_port=htons(port);
+        address_sock.sin_addr.s_addr=htonl(INADDR_ANY);
+        int r=bind(sock,(struct sockaddr *)&address_sock,sizeof(struct sockaddr_in));
+        if(r==0){
+            char tampon[256];
+            while(1){
+                int rec=recv(sock,tampon,256,0);
+                tampon[rec]='\0';
+                printf("UDP NORMAL :%s",tampon);
             }
-            return -1;
         }
     }else{
-        if(n<11){
-            return -1;
-        }
-        if(s[5] == ' '){
-            int cmp = 1;
-            for(int i = 7 ;s[i]; i++){
-                if(s[i]==' '){
-                    cmp++;
-                }
+        //Reception du multicast
+        int sock=socket(PF_INET,SOCK_DGRAM,0);
+        int ok=1;
+        int r=setsockopt(sock,SOL_SOCKET,SO_REUSEPORT,&ok,sizeof(ok));
+        struct sockaddr_in address_sock;
+        address_sock.sin_family=AF_INET;
+        address_sock.sin_port=htons(portMulti);
+        address_sock.sin_addr.s_addr=htonl(INADDR_ANY);
+        r=bind(sock,(struct sockaddr *)&address_sock,sizeof(struct sockaddr_in));
+        struct ip_mreq mreq;
+        mreq.imr_multiaddr.s_addr=inet_addr(ipMulti);
+        mreq.imr_interface.s_addr=htonl(INADDR_ANY);
+        r=setsockopt(sock,IPPROTO_IP,IP_ADD_MEMBERSHIP,&mreq,sizeof(mreq));
+        if(r == 0){
+            char tampon[100];
+            while(1){
+                int rec=recv(sock,tampon,256,0);
+                tampon[rec]='\0';
+                printf("MULTICAST :%s",tampon);
             }
-            if(cmp == 2){
-                return 1;
-            }
         }
-        return -1;
     }
 }
