cda9988f05136c5153ab207f67bda77f64196e08
Bichoumac <mr.thierrychear@gmail.com>
2018-05-10 20:27:00 +0200

    Ajout des types pour team

--- TypeMessage.java
+++ TypeMessage.java
@@ -1,22 +1,23 @@
 import java.lang.*;
 
 class TypeMessage {
-	public static final int UP = 0;
-	public static final int DOWN = 1;
-	public static final int LEFT = 2;
-	public static final int RIGHT = 3;
-	public static final int SIZE = 4;
-	public static final int LIST = 5;
-	public static final int ALL = 6;
-	public static final int SEND = 7;
-	public static final int NEW = 8;
-	public static final int REG  = 9;
-	public static final int START = 10;
-	public static final int UNREG = 11;
-	public static final int GAMES = 12;
-	public static final int QUIT = 13;
-	public static final int GLIST = 14;
-	
+    public static final int UP = 0;
+    public static final int DOWN = 1;
+    public static final int LEFT = 2;
+    public static final int RIGHT = 3;
+    public static final int SIZE = 4;
+    public static final int LIST = 5;
+    public static final int ALL = 6;
+    public static final int SEND = 7;
+    public static final int NEW = 8;
+    public static final int REG  = 9;
+    public static final int START = 10;
+    public static final int UNREG = 11;
+    public static final int GAMES = 12;
+    public static final int QUIT = 13;
+    public static final int GLIST = 14;
+    public static final int TLIST = 15;
+    public static final int CHANGETEAM = 16;
 	
     /*
       Liste des messages valides :
@@ -31,7 +32,12 @@ class TypeMessage {
       12/ GAMES?***
       13/ QUIT***
       14/ GLIST?***
+      15/ TLIST?***
+      16/ CHANGETEAM***
+      17/ NEWT id port***
+      18/ REGT id port m***
 
+     
     */        
 }
 
@@ -76,10 +82,12 @@ class Send extends TypeMessage {
 class New extends TypeMessage {
     String id;
     int port;
-
-    public New (String id, int port) {
+    boolean team; // false = solo, true = team
+    
+    public New (String id, int port, boolean f) {
 	this.id = id;
 	this.port = port;
+	this.team = f;
     }
 }
 
@@ -87,16 +95,18 @@ class Reg extends TypeMessage {
     String id;
     int port;
     int m;
-
-    public Reg (String id, int port, String m) {
+    boolean team; // false = solo, true = team
+    
+    public Reg (String id, int port, String m, boolean team) {
 	this.id = id;
 	this.port = port;
 	this.m = Integer.parseInt(m);
+	this.team = team;
     }
 }
 
 class NoArgs extends TypeMessage {
-    int type; // 10 = START, 11 = UNREG, 12 = GAMES?, 13 = QUIT, 14 = GLIST?
+    int type; // 10 = START, 11 = UNREG, 12 = GAMES?, 13 = QUIT, 14 = GLIST?, 15 = TLIST?, 16 = CHANGETEAM
     public NoArgs (int type) {
 	this.type = type;
     }
