8422b54097cbe9e8d84209efd0d04653f9d74a4c
Philippe EAR <earphilippefr@gmail.com>
2018-05-10 19:19:03 +0200

    retire println

--- Messagerie.java
+++ Messagerie.java
@@ -84,7 +84,6 @@ public boolean sendMessageEnd(Player gagnant, int score){
 public boolean sendMessageAll(String msg, Player playerFrom){
 	try{
 		String msg2 = "MESA "+playerFrom.getId()+" "+msg+"+++";
-		System.out.println("MSG UDP"+msg2);
 		byte[] data = msg2.getBytes();
 		System.out.println(ipAddress+ " "+multiPort);
 
